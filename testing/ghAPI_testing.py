b6f5d4e32e3a7242556952041c59b7a735ffde60
ia-lozano <130255731+ia-lozano@users.noreply.github.com>
2025-08-17 23:44:44 -0600

    updating

---
+++ testing/ghAPI_testing.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+# sudo /usr/bin/python3 -m pip install PyGithub
+import subprocess
+
+url = "https://raw.githubusercontent.com/ia-lozano/mir4_data/refs/heads/master/testing/nft_list_2025-06-22.csv"
+df = pd.read_csv(url)
+
+print(df.head())
+
+df_clean = df[["power", "price"]]
+
+print(df_clean.head())
+
+commands = [
+    ["git", "add", "."],
+    ["git", "commit", "-m", "updating"],
+    ["git", "push", "-u", "origin", "master"]
+]
+
+for cmd in commands:
+    subprocess.run(cmd)
+
+
+
+
